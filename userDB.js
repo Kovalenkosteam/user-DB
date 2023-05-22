eb55519e434fa7e68c19fb57d344c7b57bf2cd95
kovalenkosteam <kovalenkosteam@gmail.com>
2023-05-22 16:43:36 +0300

    first

--- userDB.js
+++ userDB.js
@@ -52,7 +52,7 @@ ShowMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
     for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt('Ваш любимый жанр под номером ' + (i+1), '');
+        personalMovieDB.genres[i] = prompt('Ваш любимый жанр под номером ' + (i + 1), '');
     }
 }
-// writeYourGenres()
+// writeYourGenres();
