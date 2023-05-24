681d1ba18a1defa668d21b76e60ef50215fd04af
kovalenkosteam <kovalenkosteam@gmail.com>
2023-05-24 14:04:02 +0300

    2.31

--- 2.29.js
+++ 2.29.js
@@ -1,11 +1,19 @@
 function calculateVolumeAndArea(num){
     if (!Number.isInteger(num)||num<0){
-        console.log('При вычислении произошла ошибка');
+        return ('При вычислении произошла ошибка');
     } else{
     return ('Объем куба: '+num*num*num+', площадь всей поверхности: '+6*num*num);
     }
 }
-console.log(calculateVolumeAndArea(15));
+console.log(calculateVolumeAndArea('-15'));
 
-
-
+function getCoupeNumber(num) {
+    if (!Number.isInteger(num) || num < 0 || typeof(num) === 'string') {
+        return ('Ошибка. Проверьте правильность введенного номера места');
+    } else if (num > 0 && num <= 36) {
+        return Math.ceil(num / 4);
+    } else {
+        return ('Таких мест в вагоне не существует');
+    }
+}
+console.log(getCoupeNumber(5));
