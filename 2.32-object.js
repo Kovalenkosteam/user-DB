58c493d58891bef103372cf9879d136009516433
kovalenkosteam <kovalenkosteam@gmail.com>
2023-05-29 16:34:18 +0300

    2.32

--- 2.32-object.js
+++ 2.32-object.js
@@ -1,12 +1,39 @@
-const option={
-    name:'test',
+const option = {
+    name: 'test',
     width: 1024,
     height: 1024,
-    colors:{
-        border:'black',
+    colors: {
+        border: 'black',
         background: 'red'
+    },
+    makeTest:function(){
+        console.log('Test');
     }
-}
-console.log(option.name);
-delete option.name;
-console.log(option);
+};
+
+option.makeTest();
+
+// const{border,background}=option.colors;
+// console.log(background);
+// console.log(Object.keys(option).length);
+// console.log(option.colors.background);
+// // console.log(option['colors']['background']);
+// // delete option.width;
+// console.log(option);
+
+// let counter=0;
+
+// for (let key in option) {
+//     if (typeof (option[key]) === 'object') {
+//         for (let i in option[key]) {
+//             console.log(`Свойство ${i}, имеет значение: ${option[key][i]}`);
+//             counter++;
+//         }
+//     }else {
+//         console.log(`Свойство ${key}, имеет значение: ${option[key]}`);
+//         counter++;
+//     }
+
+// }
+
+// console.log(counter);
