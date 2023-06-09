550acd720bfe5f3120540e1914cbed04beaf6754
kovalenkosteam <kovalenkosteam@gmail.com>
2023-06-09 04:12:15 +0300

    2.44_work_with_site

--- 2.44_project_edit/js/script.js
+++ 2.44_project_edit/js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
@@ -16,11 +17,34 @@
 
 const movieDB = {
     movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        'Логан',
+        'Лига справедливости',
+        'Ла-ла лэнд',
+        'Одержимость',
+        'Скотт Пилигрим против...'
     ]
 };
+const promo__adv = document.querySelectorAll('.promo__adv img');
+const promo__bg = document.querySelector('.promo__bg');
+let promo__genre = promo__bg.querySelector('.promo__genre');
+const list = document.querySelector('.promo__interactive-list');
+promo__adv.forEach(i => {
+    i.remove();
+});
+promo__genre.textContent = 'Драма';
+promo__bg.style.backgroundImage = 'url(./img/bg.jpg)';
+list.innerHTML = '';
+console.log(movieDB.movies);
+console.log(movieDB.movies.sort());
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    list.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+                            <div class="delete"></div>
+                        </li>`;
+});
+
+
+
+
 
