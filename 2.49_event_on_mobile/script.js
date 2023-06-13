1ee7ac2f6ec1318aec43d68329b5900e03a2ace3
kovalenkosteam <kovalenkosteam@gmail.com>
2023-06-13 08:59:13 +0300

    2.49_event_on_mobile

--- 2.49_event_on_mobile/script.js
+++ 2.49_event_on_mobile/script.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-document.addEventListener('DOMException',()=>{
-
-=======
 document.addEventListener('DOMContentLoaded',()=>{
     const box=document.querySelector('.box');
     // touch(start, end, move, enter, leave, cancel)
     box.addEventListener('touchstart',()=>{
         box.style.backgroundColor='green';
     });
->>>>>>> fc4a4e2 (2.49_event_on_mobile)
 });
