68663513de1ac7df1b4e4f5924e4e1b0f728c68a
kovalenkosteam <kovalenkosteam@gmail.com>
2023-07-01 03:39:41 +0300

    4.71_windows_parametr

---
+++ 4.71_window_parametr/script.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+'use strict';
+const box = document.querySelector('.box');
+const btn=document.querySelector('button');
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+// const width = box.offsetWidth;
+// const height = box.offsetHeight; 
+const width = box.scrollWidth;
+const height = box.scrollHeight; 
+console.log(width,height);
+
+btn.addEventListener('click',()=>{
+// box.style.height=box.scrollHeight+'px';
+console.log(box.scrollTop);
+});
+console.log(box.getBoundingClientRect());
+const style=window.getComputedStyle(box);
+console.log(style.display);
+
+document.documentElement.scrollTop=0;//возврат страницы наверх
