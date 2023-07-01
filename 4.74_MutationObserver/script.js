9ca88bffda7874446377a330bd80f6c84ee08b18
kovalenkosteam <kovalenkosteam@gmail.com>
2023-07-01 23:24:13 +0300

    4.74_MutationObserver

---
+++ 4.74_MutationObserver/script.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+'use strict';
+
+const box = document.querySelector('.box');
+let observer= new MutationObserver(mutationRecords=>{
+	console.log(mutationRecords);
+
+});
+observer.observe(box, {
+	childList: true
+});
+observer.disconnect();
